fd8240c60bc4929bb887e434592e1af40701119d
Sachinkisna <sachinsoftware0001@gmail.com>
2024-12-25 11:51:42 +0530

    updated

--- script.js
+++ script.js
@@ -6,3 +6,20 @@ let i;
 // }
 
 //for of loop
+// let name='Sachin Kumar';
+// for(let i of name){
+//     console.log(i);
+// }
+
+
+// for in loop
+
+let student={
+    name:'sachin',
+    age:34,
+    isPass:true,
+};
+
+for(let i in student){
+    console.log(i,"=",student[i]);
+}
